e0da246999187b600f40690ef90d4ef1359930ed
Jeremy <acnajcadcac.yahoo.com>
2017-02-16 13:56:15 -0600

    first commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='AsciiArt',
+    version='0.1.dev',
+    description='Ascii "art" from images',
+    author='jeremy',
+    packages=['asciiart'],
+    install_requires=['pillow', 'requests'])
